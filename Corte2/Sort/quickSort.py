700af9f79479c2ebf02216ad515609e51532fd33
Matteo Rojas Arciniegas <mrojasa@udistrital.edu.co>
2025-12-04 12:45:58 -0500

    Update quickSort.py

--- Corte2/Sort/quickSort.py
+++ Corte2/Sort/quickSort.py
@@ -1,1 +1,32 @@
+def partition(arr, low, high):
+    pivot = arr[high]
+    i = low - 1
 
+    for j in range(low, high):
+        if arr[j] < pivot:
+            i += 1
+            arr[i], arr[j] = arr[j], arr[i]
+
+    arr[i + 1], arr[high] = arr[high], arr[i + 1]
+    return i + 1
+
+def quickSort(arr, low, high):
+    if low < high:
+        pi = partition(arr, low, high)
+        quickSort(arr, low, pi - 1)
+        quickSort(arr, pi + 1, high)
+
+def printArray(arr):
+    print(*arr)
+
+if __name__ == "__main__":
+    arr = [10, 7, 8, 9, 1, 5]
+    n = len(arr)
+
+    print("Arreglo original:")
+    printArray(arr)
+
+    quickSort(arr, 0, n - 1)
+
+    print("Arreglo ordenado:")
+    printArray(arr)
