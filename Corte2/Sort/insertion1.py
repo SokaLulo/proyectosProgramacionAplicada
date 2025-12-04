410eecf963c4482f442ab952c74adc5d5e9466f6
Matteo Rojas Arciniegas <mrojasa@udistrital.edu.co>
2025-12-04 12:44:22 -0500

    Update insertion1.py

--- Corte2/Sort/insertion1.py
+++ Corte2/Sort/insertion1.py
@@ -1,1 +1,23 @@
+def insertionSort(arr):
+    n = len(arr)
 
+    for i in range(1, n):
+        key = arr[i]
+        j = i - 1
+
+        while j >= 0 and arr[j] > key:
+            arr[j + 1] = arr[j]
+            j -= 1
+
+        arr[j + 1] = key
+
+if __name__ == "__main__":
+    arr = [5, 2, 4, 6, 1]
+    
+    print("Arreglo original:")
+    print(*arr)
+
+    insertionSort(arr)
+
+    print("Arreglo ordenado:")
+    print(*arr)
