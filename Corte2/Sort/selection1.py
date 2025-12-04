d5e89e622155b82f12f88e3c25b2d4509522d6fb
Matteo Rojas Arciniegas <mrojasa@udistrital.edu.co>
2025-12-04 12:46:35 -0500

    Update selection1.py

--- Corte2/Sort/selection1.py
+++ Corte2/Sort/selection1.py
@@ -1,1 +1,27 @@
+def selectionSort(arr):
+    n = len(arr)
 
+    for i in range(n - 1):
+        minIndex = i
+
+        for j in range(i + 1, n):
+            if arr[j] < arr[minIndex]:
+                minIndex = j
+
+        if minIndex != i:
+            arr[i], arr[minIndex] = arr[minIndex], arr[i]
+
+def printArray(arr):
+    print(*arr)
+
+if __name__ == "__main__":
+    arr = [5, 3, 6, 1, 4]
+    n = len(arr)
+
+    print("Arreglo original:")
+    printArray(arr)
+
+    selectionSort(arr)
+
+    print("Arreglo ordenado:")
+    printArray(arr)
