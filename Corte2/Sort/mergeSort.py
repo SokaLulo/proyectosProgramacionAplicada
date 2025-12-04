b8f5827340c435efb06101cc767cd5ec95b4744f
Matteo Rojas Arciniegas <mrojasa@udistrital.edu.co>
2025-12-04 12:45:20 -0500

    Update mergeSort.py

--- Corte2/Sort/mergeSort.py
+++ Corte2/Sort/mergeSort.py
@@ -1,1 +1,47 @@
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
 
+    L = arr[l : l + n1]
+    R = arr[m + 1 : m + 1 + n2]
+
+    i = 0
+    j = 0
+    k = l
+
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def mergeSort(arr, l, r):
+    if l < r:
+        m = l + (r - l) // 2
+
+        mergeSort(arr, l, m)
+        mergeSort(arr, m + 1, r)
+
+        merge(arr, l, m, r)
+
+if __name__ == "__main__":
+    arr = [10, 7, 8, 9, 1, 5]
+    n = len(arr)
+
+    mergeSort(arr, 0, n - 1)
+
+    print("Arreglo ordenado:")
+    print(*arr)
